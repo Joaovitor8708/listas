86017694e5ab8aa72f5981f5b247f5337d39178b
Joaovitor8708 <vitorsantanalins@gmail.com>
2023-09-01 15:08:32 -0300

    first commit

---
+++ exer01.py
@@ -0,0 +1,11 @@
+#receber dois numeros
+#dizer o maior deles com if e else
+#mostrar resultado
+
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
+
+if  num1 > num2:
+    print(num1)
+else:
+    print(num2)
